117e8beb4608020bb078daaeca92b360b8913d9d
Braeden Morrison <b.j.morrison20@gmail.com>
2023-12-29 17:13:44 -0600

    Clean up build files and lay out makefiles
    Fix some issues with build scripts, such as an unnecessary rm in the run
    script and a missing existence check in the post-build script.
    
    Fix a type in the name of the external buildroot tree description file.
    
    Create some placeholder makefiles for building the bomb package that
    will be used for the actual game.

--- br2-external/board/bomb-board/post_build.sh
+++ br2-external/board/bomb-board/post_build.sh
@@ -3,4 +3,7 @@
 # Remove the network setup script. I don't know if there's an option to
 # prevent this from being generated in the first place, but I haven't
 # found it if it's there.
-rm $1/etc/init.d/S40network
+network_init_file=$1/etc/init.d/S40network
+if [ -f "$network_init_file" ]; then
+    rm $network_init_file
+fi
---
+++ br2-external/package/bomb/bomb.mk
@@ -0,0 +1,23 @@
+# Buildroot makefile for the binary bomb
+
+BOMB_VERSION = 0.1
+BOMB_SITE = $(BOMB_PKGDIR)
+BOMB_SITE_METHOD = local
+BOMB_INSTALL_TARGET = YES
+BOMB_INSTALL_IMAGES = YES
+
+# This command is just taken from the example, but I don't know if $(MAKE) and
+# $(TARGET_CONFIGURE_OPTS) are actually defined.
+define BOMB_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define BOMB_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0777 $(@D)/bomb $(TARGET_DIR)/usr/bin
+endef
+
+define BOMB_INSTALL_IMAGES_CMDS
+	$(INSTALL) -D -m 0444 $(@D)/guide.txt $(BINARIES_DIR)/
+endef
+
+$(eval $(generic-package))
